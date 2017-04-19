151ef2946e37626e06f41cc39c47b6c83c70b736
Benjamin Cahill <benjaminacahill@gmail.com>
2017-04-18 21:43:58 -0700

    still struggling with this assignment, but i feel it is heping me more than salmon cookies, added some js to weed out duplicates and get 3 pictures

--- app.js
+++ app.js
@@ -1,36 +1,71 @@
 'use strict';
 
-var images = [];
-
-var bag = new Image('bag', 'img/bag.jpg');
-var banana = new Image('banana', 'img/banana.jpg');
-var bathroom = new Image('bathroom', 'img/bathroom.jpg');
-var boots = new Image('boots', 'img/boots.jpg');
-var breakfast = new Image('breakfast', 'img/breakfast.jpg');
-var bubblegum = new Image('bubblegum', 'img/bubblegum.jpg');
-var chair =new Image('chair', 'img/chair.jpg');
-var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
-var dogDuck = new Image('dog duck', 'img/dog-duck.jpg');
-var dragon = new Image('dragon', 'img/dragon.jpg');
-var pen = new Image('pen', 'img/pen.jpg');
-var petSweep =new Image('pet sweep', 'img/pet.jpg');
-var scissors = new Image('scissors', 'img/scissors');
-var shark = new Image('shark', 'img/shark.jpg');
-var sweep = new Image('sweep', 'img/sweep.png');
-var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
-var unicorn = new Image('unicorn', 'img/unicorn.jpg');
-var usb = new Image('usb', 'img/usb.gif');
-var water = new Image('water', 'img/water.jpg');
-var wineGlass = new Image('wine glass', 'img/wine-glass.jpg');
-
 function Image(title, location){
   this.title = title;
-  this.location = location;
-  this.end = 0;
+  this.location = './' + location;
+  this.endCount = 0;
+  this.clickCount = 0;
 }
-Image.prototype.rng = function(){
-  min = Math.ceil(0);
-  max = Math.floor(images.length);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-console.log(images.length);
+
+var currentPage = [];
+var previousPage = [];
+var secondToLastpage = [];
+
+// an array of photos
+var images = [
+  new Image('bag', 'img/bag.jpg'),
+  new Image('banana', 'img/banana.jpg'),
+  new Image('bathroom', 'img/bathroom.jpg'),
+  new Image('boots', 'img/boots.jpg'),
+  new Image('breakfast', 'img/breakfast.jpg'),
+  new Image('bubblegum', 'img/bubblegum.jpg'),
+  new Image('chair', 'img/chair.jpg'),
+  new Image('cthulhu', 'img/cthulhu.jpg'),
+  new Image('dog-duck', 'img/dog-duck.jpg'),
+  new Image('dragon', 'img/dragon.jpg'),
+  new Image('pen', 'img/pen.jpg'),
+  new Image('pet-sweep', 'img/pet.jpg'),
+  new Image('scissors', 'img/scissors'),
+  new Image('shark', 'img/shark.jpg'),
+  new Image('sweep', 'img/sweep.png'),
+  new Image('tauntaun', 'img/tauntaun.jpg'),
+  new Image('unicorn', 'img/unicorn.jpg'),
+  new Image('usb', 'img/usb.gif'),
+  new Image('water', 'img/water.jpg'),
+  new Image('wine-glass', 'img/wine-glass.jpg'),
+];
+
+// random number generator
+function rng(list){
+  return Math.floor(Math.random() * list.length);
+}
+
+// using the rng takes the three photos and makes sure they dont repeat either on the page or on the previous page
+function threePhotos(){
+
+  images = images.concat(secondToLastpage);
+  secondToLastpage = previousPage;
+  previousPage = currentPage;
+
+  currentPage = [];
+
+  var nextPhoto = images.splice(rng(images), 1);
+  currentPage = currentPage.concat(nextPhoto);
+  nextPhoto = images.splice(rng(images), 1);
+  currentPage = currentPage.concat(nextPhoto);
+  nextPhoto = images.splice(rng(images), 1);
+  currentPage = currentPage.concat(nextPhoto);
+
+  previousPage = images.concat(currentPage);
+  return currentPage;
+}
+
+// render the images to the page
+var image1 = document.getElementById(image1);
+var image2 = document.getElementById(image2);
+var image3 = document.getElementById(image3);
+
+image1 = appendChild();
+
+// click counter that stops the the page after 25 clicks
+// and the counters for how many times a specific image is clicked
