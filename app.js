6d43d23bd443cf12b38ce59ebc62286974347f0f
Benjamin Cahill <benjaminacahill@gmail.com>
2017-04-23 12:26:55 -0700

    stuff

--- app.js
+++ app.js
@@ -52,28 +52,6 @@ function getRandomIndex(list){
   return Math.floor(Math.random() * list.length);
 }
 
-function imageClick(event) {
-  var image = event.target;
-  console.log(image);
-  var photosOnScreenIndex = image.getAttribute('photos-on-screen-index');
-  photosCurrentlyOnScreen[photosOnScreenIndex].clickCount++;
-
-  clicksRemaining--;
-
-  if(clicksRemaining > 0){
-    renderPhotoChoices();
-  } else {
-    photoConcat();
-    chartDisplay();
-    try {
-      localStorage.photos = JSON.stringify(photos);
-    } catch (error) {
-      console.log('uh-oh spagetti-o');
-    }
-  }
-}
-
-
 
 function getThreePhotos(){
 
@@ -89,11 +67,27 @@ function getThreePhotos(){
   photosCurrentlyOnScreen = photosCurrentlyOnScreen.concat(nextPhoto);
   nextPhoto = photos.splice(getRandomIndex(photos), 1);
   photosCurrentlyOnScreen = photosCurrentlyOnScreen.concat(nextPhoto);
-
-  photosOnPreviousScreen = photos.concat(photosCurrentlyOnScreen);
-  return photosCurrentlyOnScreen;
 }
 
+function imageClick(event) {
+  var image = event.target;
+  console.log(image);
+  var photosOnScreenIndex = image.getAttribute('photos-on-screen-index');
+  photosCurrentlyOnScreen[photosOnScreenIndex].clickCount++;
+
+  clicksRemaining--;
+
+  if(clicksRemaining > 0){
+    renderPhotoChoices();
+  } else {
+    renderChart();
+    try {
+      photos = JSON.stringify(localStorage.photos);
+    } catch (error) {
+      console.log('uh-oh spagetti-oh');
+    }
+  }
+}
 
 function renderPhotoChoices() {
   getThreePhotos();
@@ -110,38 +104,50 @@ function renderPhotoChoices() {
   }
 }
 
-function photoConcat(){
+function renderChart(){
   photos = photos.concat(photosCurrentlyOnScreen);
+  console.log('photos in array',photos);
+  console.log('photos currently on screen', photosCurrentlyOnScreen);
   photos = photos.concat(photosOnPreviousScreen);
+  console.log('photos on previous screen',photosOnPreviousScreen);
   photos = photos.concat(photosOnSecondToLastScreen);
-}
 
-function chartDisplay(){
-  var ctx = document.getElementById("chart-controller");
+  app.textContent = '';
+
+  var ctx = document.getElementById('chart-controller');
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: [
+        photos[0].name,
+        photos[1].name,
+        photos[2].name,
+        photos[3].name,
+        photos[4].name,
+        photos[5].name,
+        photos[6].name,
+        photos[7].name,
+        photos[8].name,
+        photos[9].name,
+        photos[10].name,
+        photos[11].name,
+        photos[12].name,
+        photos[13].name,
+        photos[14].name,
+        photos[15].name,
+        photos[16].name,
+        photos[17].name,
+        photos[18].name,
+        photos[19].name,
+      ],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
+        label: 'How many times you clicked the picture',
+        data: [
+
+       ]
+        backgroundColor: 'rgba(0, 0, 0, 1)'
+        borderColor: '#0BFF01'
+        borderWidth: 2
       }]
     },
     options: {
